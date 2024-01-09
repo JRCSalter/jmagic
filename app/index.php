dc6f8005b47f1ba2e1d9726ea51f49ab01f5b7b9
John <jrc@jrcsalter.com>
2024-01-09 20:38:12 +0000

    Committing before I do something drastic with Composer.

---
+++ app/index.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "Hello from Docker php. here. This";
+
+echo "<a href='test.php'>Test</a>";
+
+echo "<pre>";
+var_dump($_SERVER);
+echo "</pre>";
--- www/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo "Hello from Docker php. here. This";
-
-echo "<pre>";
-var_dump($_SERVER);
-echo "</pre>";
