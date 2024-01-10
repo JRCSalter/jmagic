c6ca0ed183a81177f326d10603553db372b6dd60
John <jrc@jrcsalter.com>
2024-01-10 22:48:03 +0000

    Installed Laravel

--- app/error/404.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-echo "This is a 404 error";
--- app/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo "Hello from Docker php. here. This";
-
-echo "<a href='test.php'>Test</a>";
-
-echo "<pre>";
-var_dump($_SERVER);
-echo "</pre>";
